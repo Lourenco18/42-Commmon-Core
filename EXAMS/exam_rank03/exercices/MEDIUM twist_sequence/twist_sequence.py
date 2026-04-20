cdfeda56e2d737700bcc9e6e4e0e1e70c7431590
Lourenco18 <contact.lourenco18@gmail.com>
2026-04-20 11:48:46 +0100

    some corrections

--- EXAMS/exam_rank03/exercices/MEDIUM twist_sequence/twist_sequence.py
+++ EXAMS/exam_rank03/exercices/MEDIUM twist_sequence/twist_sequence.py
@@ -1,51 +1,10 @@
-def rotate_rigth(nums,n) -> list:
-    rotated = []
-    i = len(nums) - n
-    while i < len(nums):
-        rotated.append(nums[i])
-        i +=1
-    i = 0
-    while i < (len(nums) - n):
-        rotated.append(nums[i])
-        i +=1
-    return rotated
+def twister(nums, n) -> list:
+    if len(nums) == 0:
+        return []
 
-def rotate_left(nums,n) -> list:
-    rotated = []
-    lista = []
-    n = -n
-    i = 0
-    while i < n:
-        lista.append(nums[i])
-        i +=1
-    i = n
-    while i < len(nums) :
-        rotated.append(nums[i])
-        i +=1
-    i = 0
-    while i < len(lista):
-        rotated.append(lista[i])
-        i +=1
-    return rotated
-            
-            
-def twister(nums,n)-> list:
-    result = []
-    if len(nums)>1:
-        if n <0:
-            if n > len(nums):
-                n = (len(nums)) -1
-            result = rotate_left(nums,n)
-        elif n > 0:
-            if n > len(nums):
-                n = (len(nums)) -1
-            result = rotate_rigth(nums,n)
+    n = n % len(nums)
 
-        else:
-            result = nums
-    else:
-        result = nums
-    return result
+    return nums[-n:] + nums[:-n]
 
 
 print(twister([1, 2, 3, 4, 5], 2))
